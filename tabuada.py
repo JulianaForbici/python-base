01093ee914970b4ffe7365b58352202355de9349
Juliana Forbici <105084941+JulianaForbici@users.noreply.github.com>
2025-10-08 22:13:31 -0300

    Create tabuada.py

---
+++ tabuada.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Imprime a tabuada do 1 ao 10.
+
+Tabuada do 1
+1
+2
+3
+...
+------------
+Tabuada do 2
+2
+4
+---
+------------
+"""
+__version__ = "0.1.0"
+__author__ = "Juliana"
+
+# base = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+base = list(range(1, 11))
+
+print(base)
+
