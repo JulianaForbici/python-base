ae80a3b70e65f1e37cd5d292bf6183060dab7fbb
Juliana Forbici <105084941+JulianaForbici@users.noreply.github.com>
2025-10-08 21:12:01 -0300

    Implementado hello word multi linguas

--- hello.py
+++ hello.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
-print("Hello World!")
+current_language = "en_US"
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour Monde"
+
+print(msg)
 
