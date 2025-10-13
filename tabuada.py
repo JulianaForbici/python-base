7abb3b2f28d76d459db8c788040d2a20e71bf0cc
Juliana Forbici <105084941+JulianaForbici@users.noreply.github.com>
2025-10-13 20:43:28 -0300

    Update tabuada.py

--- tabuada.py
+++ tabuada.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Imprime a tabuada do 1 ao 10.
+"""Imprime a tabuada do 1 ao 10
 
 Tabuada do 1
 1
@@ -16,16 +16,18 @@ Tabuada do 2
 __version__ = "0.1.0"
 __author__ = "Juliana"
 
-# base = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-base = list(range(1, 11))
+template = """
 
-print(base)
+---Tabuada do 2---
 
-# Iterable (percorriveis)
+##################
+"""
 
-# para cada numero em numeros:
-for numero in numeros:
-    print("Tabuada do:", numero)
-    for outro_numero in numeros:
-        print(numero * outro_numero)
-    print("------------")
+
+numeros = list(range(1, 11))
+for n1 in numeros:
+    print(f"{'-'*10} Tabuada do {n1} {'-'*10}")
+    for n2 in numeros:
+        resultado = n1 * n2
+        print(f"{n1} x {n2} = {resultado}")
+    print() 
