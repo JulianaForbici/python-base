5c07de437283b37fbf2488371df7e59cd0d15644
Juliana Forbici <105084941+JulianaForbici@users.noreply.github.com>
2025-10-13 21:10:58 -0300

    Create tupes.py

---
+++ tupes.py
@@ -0,0 +1,14 @@
+cor1 = "red"
+cor2 = "green"
+cor3= "blue"
+
+# primeiro elemento de uma sequência em python começa no 0
+
+dados = "Juliana", 15, True, None, 70.3
+
+('Juliana', 15, True, None, 70.3)
+
+# dir(tuple): dois metódos públicos somente > count e index! 
+
+dados.count("Juliana")
+
